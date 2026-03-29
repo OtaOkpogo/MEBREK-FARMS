268db3bee908699e756419441ef68ef5c9df91ac
OtaOkpogo <otaokpogo@yahoo.com>
2026-03-29 13:11:52 +0100

    updated contact,jsx

---
+++ Mebrek-farms/frontend/src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact MEBREK FARMS</h1>
+      <p>Phone: +234 903 372 3103</p>
+      <p>Email: info@mebrekfarms.com</p>
+    </div>
+  );
+}
