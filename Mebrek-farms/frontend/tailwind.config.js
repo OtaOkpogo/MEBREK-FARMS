311f991a782c24f548dcf2910aadb9281be9333a
OtaOkpogo <otaokpogo@yahoo.com>
2026-03-31 12:07:06 +0100

    updated tailwind

--- Mebrek-farms/frontend/tailwind.config.js
+++ Mebrek-farms/frontend/tailwind.config.js
@@ -3,11 +3,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#2E7D32",
-        secondary: "#6D4C41",
-        accent: "#FBC02D"
+        farmGreen: "#2f855a",
+        eggYellow: "#facc15"
       }
-    }
+    },
   },
-  plugins: []
+  plugins: [],
 };
