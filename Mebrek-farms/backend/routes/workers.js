a50ffe86b784282b6000bb6baed5ae9e2492af9e
OtaOkpogo <otaokpogo@yahoo.com>
2026-04-14 13:56:50 +0100

    updated workers routes

--- Mebrek-farms/backend/routes/workers.js
+++ Mebrek-farms/backend/routes/workers.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ message: "Workers route working" });
-});
+const auth = require("../middleware/auth");
+
+const {
+  createWorker,
+  getWorkers,
+  updateWorker,
+  deleteWorker
+} = require("../controllers/workerController");
+
+router.post("/", auth, createWorker);
+router.get("/", auth, getWorkers);
+router.put("/:id", auth, updateWorker);
+router.delete("/:id", auth, deleteWorker);
 
 module.exports = router;
