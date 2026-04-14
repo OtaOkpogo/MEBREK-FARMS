f12abcbef59ca8c5477f5faf67ee48363db89d88
OtaOkpogo <otaokpogo@yahoo.com>
2026-04-14 20:50:16 +0100

    updated frontend

--- Mebrek-farms/frontend/src/services/workerService.js
+++ Mebrek-farms/frontend/src/services/workerService.js
@@ -1,11 +1,38 @@
-import api from "./api";
+import axios from "axios";
 
-export const fetchWorkers = async () => {
-  const response = await api.get("/workers");
-  return response.data;
+const API_URL = "http://localhost:5000/api/workers";
+
+// Helper: attach token
+const getConfig = () => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      Authorization: token,
+    },
+  };
 };
 
-export const addWorker = async (workerData) => {
-  const response = await api.post("/workers", workerData);
-  return response.data;
+// ✅ Get All Workers
+export const fetchWorkers = async () => {
+  const res = await axios.get(API_URL, getConfig());
+  return res.data;
+};
+
+// ✅ Create Worker
+export const createWorker = async (workerData) => {
+  const res = await axios.post(API_URL, workerData, getConfig());
+  return res.data;
+};
+
+// ✅ Update Worker
+export const updateWorker = async (id, workerData) => {
+  const res = await axios.put(`${API_URL}/${id}`, workerData, getConfig());
+  return res.data;
+};
+
+// ✅ Delete Woker
+export const deleteWorker = async (id) => {
+  const res = await axios.delete(`${API_URL}/${id}`, getConfig());
+  return res.data;
 };
