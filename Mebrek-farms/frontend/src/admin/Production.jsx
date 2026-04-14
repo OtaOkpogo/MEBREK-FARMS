2acf5a683f6e90ca84b32570658efb066d50cc4f
OtaOkpogo <otaokpogo@yahoo.com>
2026-04-14 13:33:32 +0100

    Created Production.jsx

---
+++ Mebrek-farms/frontend/src/admin/Production.jsx
@@ -0,0 +1,8 @@
+export default function Production() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">Egg Production</h2>
+      <p>Track daily egg production here.</p>
+    </div>
+  );
+}
