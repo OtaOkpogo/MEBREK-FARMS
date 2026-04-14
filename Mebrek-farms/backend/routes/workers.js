82ad2490b5b28b5633f3c73c3557ace45ba7a8f6
OtaOkpogo <otaokpogo@yahoo.com>
2026-04-14 21:09:35 +0100

    Updated backend

--- Mebrek-farms/backend/routes/workers.js
+++ Mebrek-farms/backend/routes/workers.js
@@ -6,12 +6,14 @@ const auth = require("../middleware/auth");
 const {
   createWorker,
   getWorkers,
+  getWorkerStats,
   updateWorker,
-  deleteWorker
+  deleteWorker,
 } = require("../controllers/workerController");
 
 router.post("/", auth, createWorker);
 router.get("/", auth, getWorkers);
+router.get("/stats", auth, getWorkerStats);
 router.put("/:id", auth, updateWorker);
 router.delete("/:id", auth, deleteWorker);
 
