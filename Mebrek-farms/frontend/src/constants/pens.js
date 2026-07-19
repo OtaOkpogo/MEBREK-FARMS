e4b282965d22a4938af2ae5a3debf88dd1a29986
OtaOkpogo <otaokpogo@yahoo.com>
2026-07-19 21:54:59 +0100

    Created pens.js

---
+++ Mebrek-farms/frontend/src/constants/pens.js
@@ -0,0 +1,10 @@
+export const PENS = [
+  "Battery Cage Row 1",
+  "Battery Cage Row 2",
+  "Battery Cage Row 3",
+  "Deep Litter Pen 1",
+  "Deep Litter Pen 2",
+  "Deep Litter Pen 3",
+  "Sick Bay",
+  "Pen 150",
+];
